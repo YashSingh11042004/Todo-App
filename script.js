5c6b2e9e3ad83b8b9b8ea99bc0dd34d4eebdbd13
Yash Singh <154120524+YashSingh11042004@users.noreply.github.com>
2025-02-24 12:54:52 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
+let todo = ["Tea" , "Coffee","Pizza","Pepsi","Burger","Water"];
 
+let user = prompt("Enter to go ahead");
+
+while(true){
+          if(user == "quit"){
+              console.log("You are quitting:");
+              break;
+   }
+
+	if(user == "list"){
+	   for(task of todo){
+		console.log(task);
+		}
+	}
+	else if(user == "add"){
+	   let Adder = prompt("What you have to add Enter here:");
+	   todo.push(Adder);
+	   console.log("Successfully Added");
+	}
+	else if(user == "delete"){
+	   let delete = prompt("What you have to delete Enter index here:");
+	   todo.splice(delete, 1);
+	   console.log("Successfully delete");
+	}
+}
